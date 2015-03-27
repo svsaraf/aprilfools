bb453305f8035b60ad375a4affc0759d572318bb
Sanjay <svsaraf90@gmail.com>
2015-03-26 23:46:07 -0700

    background

--- SpeakingOutofTurn.sh
+++ SpeakingOutofTurn.sh
@@ -1,5 +1,5 @@
 Write this in mac terminal:
 
 """
-nohup bash <(while true; do say "Sanjay is the king."; sleep $(( (RANDOM % (5*60) ) + 1 )); done)
+nohup bash <(while true; do say "Sanjay is the king."; sleep $(( (RANDOM % (5*60) ) + 1 )); done) &
 """
